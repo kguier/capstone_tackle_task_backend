770272ab5c70038ffb1acb4417164bcf72c0bd6f
Kira <knguier12@gmail.com>
2024-01-18 18:51:17 -0500

    First draft of ERD submitted

---
+++ capstone_tackle_task_backend/FullStackAuth_WebAPI/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FullStackAuth_WebAPI.Models
+{
+    public class Event
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ capstone_tackle_task_backend/FullStackAuth_WebAPI/Models/Task.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FullStackAuth_WebAPI.Models
+{
+    public class Task
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
